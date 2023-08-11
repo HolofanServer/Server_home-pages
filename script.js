01eeadef164a8ea74380bb83f36596b7a5e4370f
FreeWiFi <131158009+FreeWiFi7749@users.noreply.github.com>
2023-08-11 18:40:11 -0500

    new1

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// script.js
-// 今後の拡張のためのJavaScriptコードをこちらに追加してください
+function toggleMenu() {
+    const menuModal = document.getElementById('menuModal');
+    if(menuModal.style.right === '-400px' || menuModal.style.right === "") {
+        menuModal.style.right = '0';
+    } else {
+        menuModal.style.right = '-400px';
+    }
+}
