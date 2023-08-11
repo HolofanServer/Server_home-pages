16eb92484864b84ce7a9729b9d109fa189994c17
FreeWiFi7749 <holofansaba@gmail.com>
2023-08-11 14:17:56 -0500

    add animation

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-// script.js
-// 今後の拡張のためのJavaScriptコードをこちらに追加してください
+window.onload = function() {
+    const animationElement = document.getElementById('animation');
+    setTimeout(() => {
+        animationElement.classList.add('mask-animation');
+    }, 500); // 0.5秒後にアニメーションを開始（必要に応じて調整）
+};
