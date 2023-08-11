e84088f3718802ff72b79496408c94bd55a165c3
FreeWiFi <131158009+FreeWiFi7749@users.noreply.github.com>
2023-08-11 18:39:33 -0500

    new1

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-window.onload = function() {
-    const animationElement = document.getElementById('animation');
-    setTimeout(() => {
-        animationElement.classList.add('mask-animation');
-    }, 500); // 0.5秒後にアニメーションを開始（必要に応じて調整）
-};
+// script.js
+// 今後の拡張のためのJavaScriptコードをこちらに追加してください
