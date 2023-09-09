1644b56f7591c5ae404514848f6129ad02ae1cf7
FreeWiFi <131158009+FreeWiFi7749@users.noreply.github.com>
2023-09-09 10:04:55 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menuModal = document.getElementById('menuModal');
-    if(menuModal.style.right === '-400px' || menuModal.style.right === "") {
-        menuModal.style.right = '0';
-    } else {
-        menuModal.style.right = '-400px';
-    }
-}
